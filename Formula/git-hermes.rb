5deba993a31fc3219c96ef9bbb45e19c343cc4b1
Win Cheng <winzamark12@gmail.com>
2026-03-17 22:43:03 -0700

    bump git-hermes to v0.1.2

--- Formula/git-hermes.rb
+++ Formula/git-hermes.rb
@@ -1,10 +1,10 @@
 class GitHermes < Formula
   desc "AI-powered git commit message generator"
   homepage "https://github.com/winzamark123/git-hermes"
-  url "https://github.com/winzamark123/git-hermes/releases/download/v0.1.1/git-hermes"
-  sha256 "1c240f04b3d857a09f3dad0f81e916bdcceba11079670b1b36d8230cd68bf9a1"
+  url "https://github.com/winzamark123/git-hermes/releases/download/v0.1.2/git-hermes"
+  sha256 "b7952e1319b204e48f8a8b3e3344226431d38a2d8961890bfffc35da4eb1c89a"
   license "MIT"
-  version "0.1.1"
+  version "0.1.2"
 
   def install
     bin.install "git-hermes"
